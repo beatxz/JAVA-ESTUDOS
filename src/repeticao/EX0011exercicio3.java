938686026680565d70da67ffcdeeeb1d17c38dd9
Beatriz Rodrigues <164930551+beatxz@users.noreply.github.com>
2026-02-23 10:29:47 -0300

    Novo exercicio e correção

--- src/repeticao/EX0011exercicio3.java
+++ src/repeticao/EX0011exercicio3.java
@@ -1,4 +1,23 @@
 package repeticao;
 
+import java.util.Scanner;
+
 public class EX0011exercicio3 {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Digite um número: ");
+        double num = scanner.nextDouble();
+        double soma= num;
+        double media= 0;
+
+        for (int cont = 1; cont<6; cont++){
+            System.out.println("Digite outro número: ");
+            num = scanner.nextDouble();
+            soma = soma + num;
+            media = soma/6;
+
+        }
+        System.out.println("A soma total é: " +soma);
+        System.out.println("A média total é: " +media);
+    }
 }
