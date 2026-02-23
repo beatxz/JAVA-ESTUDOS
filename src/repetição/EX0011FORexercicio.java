7396ee4304083d7b729bf4b4a380f764455ff921
Beatriz Rodrigues <164930551+beatxz@users.noreply.github.com>
2026-02-23 09:22:47 -0300

    Novo exercicio e correção

---
+++ src/repetição/EX0011FORexercicio.java
@@ -0,0 +1,30 @@
+package repetição;
+
+import java.util.Scanner;
+
+public class EX0011FORexercicio {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        int contPar = 0;
+        int contImpar = 0;
+
+        for (int cont = 1; cont <= 5; cont++) {
+            System.out.println("Digite um numero: ");
+            int num = scanner.nextInt();
+
+            if (num % 2 == 0) {
+                contPar++;
+            } else {
+                contImpar++;
+            }
+
+
+
+        }
+        System.out.println("Total números Pares: " + contPar);
+        System.out.println("Total números Impares: " + contImpar);
+    }
+}
