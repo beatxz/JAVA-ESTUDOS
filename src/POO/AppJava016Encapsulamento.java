982365aeeb5758e447670695b11f7d915dd2c5a6
Beatriz Rodrigues <164930551+beatxz@users.noreply.github.com>
2026-03-11 21:02:13 -0300

    POO exercicios

--- src/POO/AppJava016Encapsulamento.java
+++ src/POO/AppJava016Encapsulamento.java
@@ -2,9 +2,10 @@ package POO;
 
 public class AppJava016Encapsulamento {
     public static void main(String[]args){
-        PingPong pingPong = new PingPong();
         VideoGame xDevOne=new VideoGame();
-        xDevOne.jogar(new Xadrez());
+        xDevOne.ligar();
+        xDevOne.jogar(new TiroAoAlvo());
+        xDevOne.fechar();
 
 
     }
