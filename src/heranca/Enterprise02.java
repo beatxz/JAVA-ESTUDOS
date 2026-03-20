4c34f9a07d325f314321ed7c23a62fde8f190a29
Beatriz Rodrigues <164930551+beatxz@users.noreply.github.com>
2026-03-20 18:01:47 -0300

    Herança+toString exercicio

--- src/heranca/Enterprise02.java
+++ src/heranca/Enterprise02.java
@@ -4,7 +4,7 @@ public class Enterprise02 {
     public static void main(String[]args){
         Manager02 manager = new Manager02("Isabela",1500);
         System.out.println(manager);
-        Developer developer = new Developer("bea",1000);
+        Developer developer = new Developer("bea",10500);
         System.out.println(developer);
     }
 }
