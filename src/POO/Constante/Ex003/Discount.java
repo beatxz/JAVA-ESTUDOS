dd675f399b237ced76f8a68b02969673fba07c69
Beatriz Rodrigues <164930551+beatxz@users.noreply.github.com>
2026-03-30 16:24:44 -0300

    Poo Constantes exercicio

---
+++ src/POO/Constante/Ex003/Discount.java
@@ -0,0 +1,18 @@
+package POO.Constante.Ex003;
+
+public class Discount {
+    public static final double DISCOUNT = 0.10;
+    public static final double SHIPPING = 15;
+    public static final double TAX = 0.05;
+
+    public static double applyDiscount(double product){
+        return product-(product*DISCOUNT);
+    }
+    public static double applyTax(double value){
+        return value+(value*TAX);
+    }
+    public static double applyShipping(double value){
+        return value+SHIPPING;
+    }
+
+}
---
+++ src/POO/Constante/Ex003/DiscountMain.java
@@ -0,0 +1,23 @@
+package POO.Constante.Ex003;
+
+import java.util.Scanner;
+
+public class DiscountMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the purchase amount: ");
+        double product = scanner.nextDouble();
+
+        double valueWithDiscount = Discount.applyDiscount(product);
+        double valueWithTax = Discount.applyTax(valueWithDiscount);
+        double valueWithShipping = Discount.applyShipping(valueWithTax);
+
+        System.out.printf("Value with discount: $%.2f%n",valueWithDiscount);
+        System.out.printf("Value including tax: $%.2f%n",valueWithTax);
+        System.out.printf("Value With Shipping: $%.2f%n",valueWithShipping);
+
+        scanner.close();
+
+    }
+}
