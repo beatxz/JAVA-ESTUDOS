bc7c5d58c2c1b8e735a253b07c26b7be7b58db65
Beatriz Rodrigues <164930551+beatxz@users.noreply.github.com>
2026-04-14 09:34:38 -0300

    Poo Polimorfismo exercicio

---
+++ src/POO/Polimorfismo/Dominio/Ex001/ProductTest02.java
@@ -0,0 +1,16 @@
+package POO.Polimorfismo.Dominio.Ex001;
+
+public class ProductTest02 {
+    public static void main(String[] args) {
+        Product product = new Computer("Ryzen",1500);
+        System.out.println(product.getName());
+        System.out.println(product.getPrice());
+        System.out.println(product.calculateTax());
+
+        Product product2 = new Tomato("Yellow",10);
+        System.out.println(product2.getName());
+        System.out.println(product2.getPrice());
+        System.out.println(product2.calculateTax());
+
+    }
+}
