93e1765638794de2639d73782d8cd395f0e77457
Beatriz Rodrigues <164930551+beatxz@users.noreply.github.com>
2026-04-30 15:48:33 -0300

    Poo exercicio

---
+++ src/Wrapper/Ex001/Test.java
@@ -0,0 +1,18 @@
+package Wrapper.Ex001;
+
+public class Test {
+    public static void main(String[]args){
+        Byte byteW = 127;
+        Short shortW = 1;
+        Integer intW = 1;
+        Long longW = 10L;
+        Float floatW = 10F;
+        Double doubleW = 10D;
+        Character charW = 'W';
+        Boolean booleanJ = false;
+
+        Integer intW2 = Integer.parseInt("1");
+        System.out.println(Character.isDigit('9'));
+
+    }
+}
---
+++ src/Wrapper/Ex002/String01.java
@@ -0,0 +1,13 @@
+package Wrapper.Ex002;
+
+public class String01 {
+    public static void main(String[]args){
+        String name = "Isabela";
+        String name2 = "Isabela";
+        name = name.concat(" Bea");
+        System.out.println(name);
+        System.out.println(name==name2);
+
+
+    }
+}
