51ee7fa93013771a7ca5d0ddd3c087408bb8450d
Beatriz Rodrigues <164930551+beatxz@users.noreply.github.com>
2026-05-05 11:00:43 -0300

    Wrapper exercicio

--- src/Wrapper/Ex005/NumberFormat01.java
+++ src/Wrapper/Ex005/NumberFormat01.java
@@ -5,15 +5,16 @@ import java.util.Locale;
 
 public class NumberFormat01 {
     public static void main(String[]args){
-        Locale localeDefault = Locale.getDefault();
+        Locale localeBR = Locale.forLanguageTag("pt-BR");
         Locale localeJP = Locale.JAPAN;
         Locale localeIT = Locale.ITALY;
 
         NumberFormat[] nfa = new NumberFormat[4];
-        nfa[0] = NumberFormat.getInstance(localeJP);
-        nfa[1] = NumberFormat.getInstance(localeIT);
-        nfa[3] = NumberFormat.getInstance(localeDefault);
-        double valor = 10_000.2130;
+        nfa[0] = NumberFormat.getCurrencyInstance();
+        nfa[1] = NumberFormat.getCurrencyInstance(localeJP);
+        nfa[2] = NumberFormat.getCurrencyInstance(localeIT);
+        nfa[3] = NumberFormat.getCurrencyInstance(localeBR);
+        double valor = 100_000_000.2130;
         for (NumberFormat numberFormat : nfa){
             System.out.println(numberFormat.format(valor));
         }
---
+++ src/Wrapper/Ex005/SimpleDateFormat01.java
@@ -0,0 +1,12 @@
+package Wrapper.Ex005;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class SimpleDateFormat01 {
+    public static void main(String[]args){
+        String pattern = " 'Amsterdam' dd 'de' MMMM 'de' yyyy";
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+        System.out.println(sdf.format(new Date()));
+    }
+}
