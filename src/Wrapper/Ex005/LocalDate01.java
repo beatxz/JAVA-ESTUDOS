f92bec87d4664c9114101efe88fe526d9ffae8e5
Beatriz Rodrigues <164930551+beatxz@users.noreply.github.com>
2026-05-06 14:41:12 -0300

    Wrapper exercicio

--- src/Wrapper/Ex005/LocalDate01.java
+++ src/Wrapper/Ex005/LocalDate01.java
@@ -2,17 +2,17 @@ package Wrapper.Ex005;
 
 import java.time.LocalDate;
 import java.time.Month;
-import java.util.Calendar;
-import java.util.Date;
+
 
 public class LocalDate01 {
     public static void main(String[]args){
-        System.out.println(new Date());
-        System.out.println(Calendar.getInstance());
-        System.out.println(Month.JANUARY.getValue());
-        LocalDate date = LocalDate.of(2000,Month.MAY,27);
+
+       LocalDate date = LocalDate.of(2006, Month.MAY,11);
+        LocalDate now = LocalDate.now();
         System.out.println(date.getDayOfWeek());
         System.out.println(date.lengthOfMonth());
         System.out.println(date.isLeapYear());
+        System.out.println(date);
+        System.out.println(now);
     }
 }
